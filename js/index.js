ebe0fdc3ded396209b8fbd2eaf0a0ee8026b34c3
Iva <84078385+Mistymos3@users.noreply.github.com>
2021-09-03 02:52:15 -0700

    fixed slider

--- js/index.js
+++ js/index.js
@@ -10,18 +10,27 @@ function doColor() {
     cnvs.style.backgroundColor = colorInput.value;
 }
 
-// function doSquare() {
-//     var sliderInput = document.getElementById("sldr");
-//     var len = sliderInput.value;
-//     var canvas = document.getElementById("canvas");
-//     var context = canvas.getcontext("2d");
+function doSquare() {
+    var dd1 = document.getElementById("canvas");
+    var sizeinput =
+    document.getElementById("sldr");
+    var size = sizeinput.value;
+    var context = dd1.getContext("2d");
+    context.fillStyle = "yellow";
+    context.fillRect(20, 20, size, size);
+    
+ 
+    // var sliderInput = document.getElementById("sldr");
+    // var len = sliderInput.value;
+    // var canvas = document.getElementById("canvas");
+    // var context = canvas.getcontext("2d");
 
-//     // //will clear the previous drawing
-//     // context.clearRect(0,0, square.width, square.height);
+    // // //will clear the previous drawing
+    // // context.clearRect(0,0, square.width, square.height);
 
-//     context.fillStyle="gray";
-//     context.fillRect(10,10,size,size);
-//     context.fillRect(10,10,len,len);
-//     context.fillRect(parseInt(len)+20,10,len,len);
-//     context.fillRect(len*3,10,len,len);
-// }
+    // context.fillStyle = "gray";
+    // context.fillRect(10, 10, size, size);
+    // context.fillRect(10, 10, len, len);
+    // context.fillRect(parseInt(len)+20, 10, len, len);
+    // context.fillRect(len*3, 10, len, len);
+}
